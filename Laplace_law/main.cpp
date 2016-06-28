aa7298b73ffa3b48e9f026ec0a355e573cd88a1d
YuTao <lucciola2015x@gmail.com>
2016-06-28 11:06:08 +0800

    Update main.cpp

--- Laplace_law/main.cpp
+++ Laplace_law/main.cpp
@@ -1,7 +1,7 @@
 //Author: Yu Tao ,Huazhong University of Science and Technology, Wuhan, China 2016.6.1
-//Email£ºlucciola2015x@gmail.com
+//EmailÂ£Âºlucciola2015x@gmail.com
 //In order to validate the model for Shan-chen model about Multicomponent&Multiphase fluid
-//I design a stationary bubble to validate the Laplace law
+//I design a stationary bubble to validate the Laplace law based on Chen Li's FORTRAN code
 #include <iostream>
 #include<cmath>
 #include<cstdlib>
